d9b7647fa8e713ec99e0eb69c2d008e7098f1090
WILLIAMS Sally L <w010378c@student.staffs.ac.uk>
2017-03-14 15:51:06 +0000

    service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,11 +1,18 @@
+var responseContent = '<html>' +
+  '<body>' +
+  '<style>' +
+  'body {text-align: center; background-color: #333; color: #eee;}' +
+  '</style>' +
+  '<h1>Paddys page</h1>' +
+  '<p>There seems to be a problem with your connection.</p>' +
+  '<p>Come visit us at Fred Street for free WiFi.</p>' +
+  '</body>' +
+  '</html>';
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
-      return new Response(
-        'Welcome to the our service worker test \n'+        
-'There seems to be a problem with your connection.\n'+        
-'We look forward to telling you about Paddy as soon as you are online'
-      );
+      return new Response(responseContent, {headers: {'Content-Type': 'text/html'}});
     })
   );
 });
