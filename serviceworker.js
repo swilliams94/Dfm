20625d252c25eadb16929e182a410305f7fb4ddf
WILLIAMS Sally L <w010378c@student.staffs.ac.uk>
2017-03-21 15:26:46 +0000

    serviceworker

--- serviceworker.js
+++ serviceworker.js
@@ -1,33 +1,16 @@
-var CACHE_NAME = 'gih-cache';
-var CACHED_URLS = [
- 'mystyles.css',
- 'styles.css',
- 'offline.html', 
- 'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en',
- 'https://fonts.googleapis.com/icon?family=Material+Icons',
- 'https://code.getmdl.io/1.3.0/material.teal-red.min.css'    
-    
-    
-];
-
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(CACHED_URLS);
-    })
-  );
+  console.log('install');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('activate');
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get('accept').includes('text/html')) {
-          return caches.match('offline.html');
-        }
-      });
-    })
-  );
+  if (event.request.url.indexOf('material.teal-red.min.css') !== -1) {
+    console.log('Fetch request for:', event.request.url);
+    event.respondWith(new Response('header {background: green url("")!important}', {
+      headers: { 'Content-Type': 'text/css' }
+    }));
+  }
 });
