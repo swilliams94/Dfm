7fdc61927c60be4178d34e6ba70f26fda45fa0d9
WILLIAMS Sally L <w010378c@student.staffs.ac.uk>
2017-03-28 16:40:50 +0100

    new

--- serviceworker.js
+++ serviceworker.js
@@ -1,30 +1,31 @@
 var CACHE_NAME = 'gih-cache-v5';
+var BASE_PATH = '/first.html/'
 var CACHED_URLS = [
   // Our HTML
   'first.html',
   // Stylesheets and fonts
-    'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'min-style.css',
-    'styles.css',
-    'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
+    BASE_PATH +'https://fonts.googleapis.com/icon?family=Material+Icons',
+    BASE_PATH +'min-style.css',
+    BASE_PATH +'styles.css',
+    BASE_PATH +'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
     
   // JavaScript
-    'manifest.json',
-    'material.js',
+    BASE_PATH +'manifest.json',
+    BASE_PATH +'material.js',
   // Images
-    'appimages/paddy.jpg',
-    'eventsimages/example-work01.jpg',
-    'eventsimages/example-work07.jpg',
-    'eventsimages/example-work02.jpg',
-    'eventsimages/example-work03.jpg',
-    'eventsimages/example-work04.jpg',
-    'eventsimages/example-work08.jpg',
-    'appimages/android-icon-36x36.png',
-    'appimages/android-icon-48x48.png',
-    'appimages/android-icon-72x72.png',
-    'appimages/android-icon-96x96.png',
-    'appimages/android-icon-144x144.png',
-    'appimages/android-icon-192x192.png',
+    BASE_PATH +'appimages/paddy.jpg',
+    BASE_PATH +'eventsimages/example-work01.jpg',
+    BASE_PATH +'eventsimages/example-work07.jpg',
+    BASE_PATH +'eventsimages/example-work02.jpg',
+    BASE_PATH +'eventsimages/example-work03.jpg',
+    BASE_PATH +'eventsimages/example-work04.jpg',
+    BASE_PATH +'eventsimages/example-work08.jpg',
+    BASE_PATH +'appimages/android-icon-36x36.png',
+    BASE_PATH +'appimages/android-icon-48x48.png',
+    BASE_PATH +'appimages/android-icon-72x72.png',
+    BASE_PATH +'appimages/android-icon-96x96.png',
+    BASE_PATH +'appimages/android-icon-144x144.png',
+    BASE_PATH +'appimages/android-icon-192x192.png',
 ];
 
 self.addEventListener('install', function(event) {
@@ -35,10 +36,11 @@ self.addEventListener('install', function(event) {
     })
   );
 });
+});
 
 self.addEventListener('fetch', function(event) {
   var requestURL = new URL(event.request.url);
-  if (requestURL.pathname === 'first.html') {
+  if (requestURL.pathname === BASE_PATH +'first.html') {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
         return cache.match('first.html').then(function(cachedResponse) {
