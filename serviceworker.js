8314f8998bb37b40fc0c509b9e60c73f03e8c381
WILLIAMS Sally L <w010378c@student.staffs.ac.uk>
2017-04-04 15:19:19 +0100

    new

--- serviceworker.js
+++ serviceworker.js
@@ -1,46 +1,74 @@
-var CACHE_NAME = 'gih-cache-v5';
-var BASE_PATH = '/Dfm/';
+var BASE_PATH = '/NAMEOF REPRO/';
+var CACHE_NAME = 'gih-cache-v6';
 var CACHED_URLS = [
-  // Our HTML
-  'first.html',
-  // Stylesheets and fonts
-    BASE_PATH +'https://fonts.googleapis.com/icon?family=Material+Icons',
-    BASE_PATH +'min-style.css',
-    BASE_PATH +'styles.css',
-    BASE_PATH +'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
+    // Our HTML
+    BASE_PATH + 'first.html',
     
-  // JavaScript
-    BASE_PATH +'manifest.json',
-    BASE_PATH +'material.js',
-  // Images
-    BASE_PATH +'appimages/paddy.jpg',
-    BASE_PATH +'eventsimages/example-work01.jpg',
-    BASE_PATH +'eventsimages/example-work07.jpg',
-    BASE_PATH +'eventsimages/example-work02.jpg',
-    BASE_PATH +'eventsimages/example-work03.jpg',
-    BASE_PATH +'eventsimages/example-work04.jpg',
-    BASE_PATH +'eventsimages/example-work08.jpg',
-    BASE_PATH +'appimages/android-icon-36x36.png',
-    BASE_PATH +'appimages/android-icon-48x48.png',
-    BASE_PATH +'appimages/android-icon-72x72.png',
-    BASE_PATH +'appimages/android-icon-96x96.png',
-    BASE_PATH +'appimages/android-icon-144x144.png',
-    BASE_PATH +'appimages/android-icon-192x192.png',
+    // Images for favicons
+    BASE_PATH + 'appimages/android-icon-36x36.png',
+    BASE_PATH + 'appimages/android-icon-48x48.png',
+    BASE_PATH + 'appimages/android-icon-72x72.png',
+    BASE_PATH + 'appimages/android-icon-96x96.png',
+    BASE_PATH + 'appimages/android-icon-144x144.png',
+    BASE_PATH + 'appimages/android-icon-192x192.png',
+    BASE_PATH + 'appimages/favicon-32x32.png',
+
+    //Images for page
+    BASE_PATH + 'appimages/offlinemap.jpg',
+    BASE_PATH + 'appimages/dino.png',
+    BASE_PATH + 'appimages/jack.jpg',
+    BASE_PATH + 'appimages/paddy.jpg',
+    BASE_PATH + 'appimages/favicon.ico',
+    BASE_PATH + 'appimages/favicon-16x16.png',
+    BASE_PATH + 'appimages/favicon-32x32.png',
+    BASE_PATH + 'appimages/favicon-96x96.png',
+    BASE_PATH + 'appimages/ms-icon-70x70.png',
+    BASE_PATH + 'appimages/ms-icon-144x144.png',
+    BASE_PATH + 'appimages/ms-icon-150x150.png',
+    BASE_PATH + 'appimages/ms-icon-310x310.png',
+    BASE_PATH + 'eventsimages/example-blog01.jpg',
+    BASE_PATH + 'eventsimages/example-blog02.jpg',
+    BASE_PATH + 'eventsimages/example-blog03.jpg',
+    BASE_PATH + 'eventsimages/example-blog04.jpg',
+    BASE_PATH + 'eventsimages/example-blog05.jpg',
+    BASE_PATH + 'eventsimages/example-blog06.jpg',
+    BASE_PATH + 'eventsimages/example-blog07.jpg',
+    BASE_PATH + 'eventsimages/example-work01.jpg',
+    BASE_PATH + 'eventsimages/example-work02.jpg',
+    BASE_PATH + 'eventsimages/example-work03.jpg',
+    BASE_PATH + 'eventsimages/example-work04.jpg',
+    BASE_PATH + 'eventsimages/example-work05.jpg',
+    BASE_PATH + 'eventsimages/example-work06.jpg',
+    BASE_PATH + 'eventsimages/example-work07.jpg',
+    BASE_PATH + 'eventsimages/example-work08.jpg',
+    BASE_PATH + 'eventsimages/example-work09.jpg',  
+    // JavaScript
+    BASE_PATH + 'offline-map.js',
+    BASE_PATH + 'material.js',
+    // Manifest
+    BASE_PATH + 'manifest.json',
+  // CSS and fonts
+    'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    BASE_PATH + 'min-style.css',
+    BASE_PATH + 'styles.css'
 ];
 
+var googleMapsAPIJS = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDMjA8BW3rCeRRGIYtMFvKU-vEEyj1CU0Y&callback=initMap';
+
 self.addEventListener('install', function(event) {
-  // Cache everything in CACHED_URLS. Installation will fail if something fails to cache
+  // Cache everything in CACHED_URLS. Installation fails if anything fails to cache
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(CACHED_URLS);
     })
   );
 });
-});
 
 self.addEventListener('fetch', function(event) {
   var requestURL = new URL(event.request.url);
-  if (requestURL.pathname === BASE_PATH +'first.html') {
+  // Handle requests for index.html
+  if (requestURL.pathname === BASE_PATH + 'first.html') {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
         return cache.match('first.html').then(function(cachedResponse) {
@@ -52,14 +80,25 @@ self.addEventListener('fetch', function(event) {
         });
       })
     );
+ // Handle requests for Google Maps JavaScript API file
+  } else if (requestURL.href === googleMapsAPIJS) {
+    event.respondWith(
+      fetch(
+        googleMapsAPIJS+'&'+Date.now(),
+        { mode: 'no-cors', cache: 'no-store' }
+      ).catch(function() {
+        return caches.match('offline-map.js');
+      })
+    );
   } else if (
     CACHED_URLS.includes(requestURL.href) ||
-    CACHED_URLS.includes(requestURL.pathname)) {
+    CACHED_URLS.includes(requestURL.pathname)
+  ) {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
         return cache.match(event.request).then(function(response) {
           return response || fetch(event.request);
-        })
+        });
       })
     );
   }
@@ -79,6 +118,7 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
+
 
 
 
